2309f6fac58aeafb6a7faae67f50cee1d77c6fa9
Gökhan <gn.ozdemir@gmail.com>
2023-03-22 11:56:05 +0300

    Update README.md files uploaded
    ilk commit
    
    kod kontrol fixed
    typo fixes
    Missing start command added
    Update index.js
    Update index.js
    Update Readme and visuals
    
    Update index.js wording
    
    
    Update README.md
    Update README.md
    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# FSWeb-S1G2-Projesi-Javascripte-giris
+# JavaScript'e Giriş
+
+Bu "sprint 1 - gün 2" projesi görevini kendi başına yapman beklenmektedir. Daha önce eğitmen eşliğinde adım adım yapılan projenin biraz daha genişletilmiş halidir.
+
+## JavaScript Temelleri
+
+- let, const, var kullanın ve farklarını gör.
+- farklı operatörleri anla ve kullan.
+- basit bir loop / while loop döngüsü yaz.
+- if/else ifadeleri kullanarak kontroller yaz.
+- function, declarations, expressions ve arrow functions kullan ve farklarını anlat.
+
+## Giriş
+
+Bugün, 7 tane JavaScript görevi tamamlarken JavaScript'in temellerini deneyimleyeceksin. Yavaş yavaş Vanilla JavaScript'e aşina oluyorsun. 🍦
+
+Alıştırmalarının yönergesini `index.js` dosyasında bulacaksın. Her görev için verilen talimatları dikkatlice oku ve görevleri tamamla. Görevi tamamlamak için gereken bilgiyi bilmediğin durumlarda Google'lama 🔍 yeteneğin ile araştırman gerektiğini unutma.
+
+## Talimatlar
+
+### Görev 1: Projenin Kurulumu
+
+VSCode, Codegrade ve Command Line araçları kullanarak:
+
+1. Bu repo'yu fork'la.
+2. Fork'ladığın repo'yu bilgisayarında clone'la.
+3. Bu projede `main` branch'ine push'layacaksın. LÜTFEN BRANCH OLUŞTURMA!
+4. **Repo'nuzun olduğu klasöre gidin** (`cd REPO_ADI`)
+5. Vs Code'da terminali açın ve `npm install` yazın ve çalıştırın. Bunu her projede sadece 1 kere yapacaksın.
+not: Bugünkü projede pull request **atmıyorsun**. 
+
+### Görev 2: MVP
+
+#### Test Etme & Debug Yapma
+
+Projen klasöründeyken, VS Code'da "split terminal"i tıklayarak ikinci bir terminal ekranı aç.
+
+![alt text](assets/split_terminal.png "Split Terminal")
+
+Sonra `npm run test` yazın ve çalıştırın
+![alt text](assets/npm_run_test.png "npm run test")
+
+Açılan ikinci terminalde `npm start` yazıp çalıştırın.
+![alt text](assets/npm_start.png "npm start")
+
+Split terminal ile ekranında testleri çalıştırırken bir diğerinde ise debug yapabileceksin. Kodlarında güncelleme yaptıkça ilerlemeyi kontrol için `console.log`'u kullanabilirsin.
+
+![alt text](assets/debug_terminal.png "Terminal buna benzemeli")
+
+`index.js` dosyasında ifade edildiği gibi verilen görevleri adım adım yap.
+**Amacın 24 testi de geçmek.**
+
+![alt text](assets/npm_test_skoru.png) "npm test skoru örneği (sadece 1 testten geçilmiş(passed)23 test başarısız (_failed_) olmuş
+
+### Görev 3: Bonus Çalışmalar
+
+Sana verilen gün sonu projesindeki görevleri tamamladıktan, yine `index.js` sonundaki bonus çalışmaları yapmanı tavsiye ederiz.
+
+## Proje Teslim Etme Adımları
+
+Verilen görevi tamamladıktan sonra [Workintech Platformu](https://app.workintech.com.tr)'nda (app.workintech.com.tr) `Sprint 1>Gün 2`'nin sonuna eklenmiş olan "` Görev: Sprint 1'in 2. Gün projesini buraya yükle`" içeriğini açıp. GitHub hesabında oluşturduğunuz repo'nun adresini buraya ekleyiniz. Eğitmenlerimiz, gün projenizi kontrol edecek ve ertesi gün size slack üzerinden geribildirimde bulunacaklar.
---
+++ index.js
@@ -0,0 +1,201 @@
+/* Görev 1: Isınalım Biraz :) */
+
+/* TÜM CEVAPLARINI BİR FUNCTION İÇİNDE VERDİĞİNİZ EMİN OL (GÖREV 1D - GÖREV 6). AKSİ TAKTİRDE, OTOMATİK TEST SİSTEMİ ÇALIŞMAYACAKTIR. */
+
+// Görev 1a, 1b ve 1c puanlamaya dahil değildir
+
+/*
+Bu görevleri yaparken, yazdığın kod'un doğru çıktı verdiğini kontrol etmek için console.log'u kullanmanı öneririz.
+Bunun için cevabı direk console'a logla veya çağırdığın fonksiyondan dönen değeri console'a logla.  
+Örneğin:  console.log(ornekFonksiyon(deger1,deger2))
+*/
+
+/*
+Görev 1a - Ehliyet Yaşı (puanlamaya dahil değildir)
+
+Aşağıdakileri yap:   
+   1. surucuYasi isimli bir değişken yaz ve bir sayı ata.
+   2. Eğer yaş 18'den büyük ise Console'da true, değilse false değeri yazdırın.
+
+   İPUCU: fonksiyon oluşturmaya gerek yok
+*/
+
+/*
+Görev 1b - Değerler (puanlamaya dahil değildir)
+
+Aşağıdakileri yap:   
+   1. 2 değişken tanımla ve bunlara değerler ata (isimlendirme olarak birinciDeger ikinciDegeri kullanabilirsin)
+   2. 1. değeri 2. değer ile karşılaştıran bir koşul oluşturun
+   3. 2. adımdaki koşul doğru olduğunda 1. değeri değiştirin
+   4. 1. değeri Console'a yazdırın
+
+   İPUCU: fonksiyon oluşturmaya gerek yok
+*/
+
+/*
+Görev 1c - String bir değeri Number'a dönüştürün (puanlamaya dahil değildir)
+
+Aşağıdakileri yap:   
+   1. String tipinde ve değeri "1999" olan bir değişken oluşturun
+   2. Değeri "1999" olan bu String'i değeri 1999 olan bir integer'a dönüştürün
+   3. Sonucu Console'a yazdırın
+
+   İPUCU: Number metoduna bakabilirsin
+*/
+
+/*
+Görev 1d - Çarpma
+ 
+Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:   
+   1. 2 argüman alsın(a ve b olarak tanımlayabilirsin)
+   2. a ve b'yi çarpıp, sonucu dönsün
+   3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
+*/
+
+function carpma(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Görev 2 : Köpeğin Yaşı */
+
+/*
+Aşağıdakileri yap:
+ 1. Aşağıdaki kopeginYasi fonsiyonunu bir argüman alacak şekilde tanımla.
+ 2. Gelen değeri kullanarak köpeğin yaşını hesapla (insanlarda 1 yıl köpeklerde 7 yıla denk gelir)
+ 3. Hesaplanan köpeğin yaşını dönün.
+ */
+
+function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Görev 3 */
+/*
+Aşağıdakileri oyun isimli fonksiyonu kullanarak yap.
+1. 2 argüman alın: oyuncunun tercihini alan bir string değer: "Taş", "Kağıt" veya "Makas" 
+   ve bilgisayarınkini alan: "Taş", "Kağıt" veya "Makas"
+   Not: string değerlerin yazımına dikkat! İlk harf büyük ve türkçe karakterler kullanarak. Yoksa testi geçemez.
+2. Aşağıdaki oyun kurallarına göre oyuncunun kazanıp kazanamadığını veya berabere kalıp kalmadığını dönün - (uygulamanın oluşturduğu çıktı tam olarak aşağıdakinin aynısı olmalı)
+ - kazanma durumunda "Kazandın!" dönmeli
+ - kaybetme durumunda "Kaybettin!" dönmeli
+ - beraberlikte "Beraberlik" dönmeli
+
+OYUNUN KURALLARI: Makas Kağıdı yener| Kağıt Taşı yener | Taş Makas'ı yener | veya beraberlik olur.
+*/
+
+function oyun(oyuncu, bilgisayar) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+// Şimdi Taş, Kağıt, Makas oyununu bilgisayara karşı oynayalım!
+/*
+Öncelikle aşağıdakileri yap:
+1. Bilgisayarın seçimini rastgele oluşturacağım bir fonksiyon tanımla. Örn: 
+   function bilgisayarinSecimi() {
+   
+   }
+2. Fonsiyonun içinde bilgisayarın seçimi için bir değişken tanımla
+3. Math.random'ı kullanarak bilgisayarın seçimini oluşturun (Math.random 0-1 arasında bir değer verecek)
+4. Bu rastgele değeri "Taş", "Kağıt" veya "Makas"a dönüştüren bir koşul oluşturun
+5. Oluşan değeri geri dönün
+
+Şimdi kendi seçtiğin bir seçime karşı bilgisayarın rastgele oluşturduğu seçimi yukarıda yazdığın oyun fonsiyonu ile oynayın ve sonucu console'a yazdırın.
+Örn: console.log(oyun("Makas",bilgisayarinSecimi()))
+*/
+
+/* Görev 4 : Metrik Dönüştürücü */
+
+//Görev 4a - Kilometreden Mil
+/*
+Aşağdaki milDonusturucu fonksiyonunu aşağıdakileri kullanarak tamamla:
+1. Kilometre değerini argüman olarak alın.
+2. Aldığın bu değeri Mil'e dönüştürün
+3. Mil değerini geri dönün
+*/
+
+function milDonusturucu(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+//Görev 4b - Santimetreden Feet
+/*
+Aşağıdakileri feetDonusturucu fonsiyonunu kullanarak yap:
+1. Santimetre değerini argüman olarak alın.
+2. Aldığın bu değeri feet'e dönüştürün
+3. feet değerini geri dönün
+
+Google'da arama ipucu: "feet cm dönüştürme"
+*/
+
+function feetDonusturucu(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Görev 5 : 5 küçük maymun yatakta zıplamış şarkısını çocuklar için hazırladığımı varsayalım. https://www.youtube.com/watch?v=e4EJ34xnlxk */
+
+/*
+Aşağıdakileri cocukSarkisi fonksiyonunda yap:
+1. Başlangıçta var olan maymun sayısını alın.
+2. cocukSarkisi fonksiyonu aşağıdaki satırı sadece 1 kere yazacak şekilde kodunuzu yazın:
+
+    "{sayı} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kmış: Bir daha yatakta zıplamak yok!"
+
+3. Bu fonksiyonun dışında bir yerde, maymun sayısının her seferinde 1 azaldığı ve maymun sayısı 1 olana kadar devem eden bir döngü oluşturun. 
+4. Bu döngüde, her seferinde cocukSarkisi fonsiyonu çalışsın ve console.log'a dönen metni yazdırsın.
+*/
+
+function cocukSarkisi(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Görev 6 : Not Hesaplayıcı */
+
+/*
+Aşağdakileri notHesapla fonksiyonunda yap.
+1. 100'lük sistemde bir sınav sonucu alın.
+2. Aşağıdaki tabloya göre notu dönün.
+
+ 90-100 arasında 'A aldın' 
+ 80-89 arasında 'B aldın'
+ 70-79 arasında 'C aldın'
+ 60-69 arasında 'D aldın'
+ daha aşağıda 'F aldın'
+ dönün
+*/
+
+function notHesapla(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Bonus Çalışma: Sesli harf sayacı - Kaç tane sesli harf var? */
+
+/*
+Aşağıdakileri sesliHarfSayaci fonskiyonunda yap.
+1. Bir argüman alın, string olsun.
+2. Bu string'in içindeki sesli harfleri sayın ve sayısını geri dönün.  (hem büyük hem de küçük harflerin sayısını dönmeli).
+
+İPUCU - yarın işlenecek array(dizi) konusunu önden araştırman gerekecek. (https://www.w3schools.com/js/js_arrays.asp)
+İPUCU - .includes() methoduna bakabilirsin. (https://www.w3schools.com/jsref/jsref_includes.asp)
+*/
+
+function sesliHarfSayaci(/*buraya kodunu yazabilirsin*/) {
+  /*buraya kodunu yazabilirsin*/
+}
+
+/* Lütfen bu satırın alt tarafını değiştirmeyin */
+function sa() {
+  console.log("Kodlar çalışıyor");
+  return "as";
+}
+sa();
+/* Bu satırdan sonrasını değiştirmeyin */
+module.exports = {
+  sa,
+  carpma,
+  kopeginYasi,
+  oyun,
+  milDonusturucu,
+  feetDonusturucu,
+  cocukSarkisi,
+  notHesapla,
+};
